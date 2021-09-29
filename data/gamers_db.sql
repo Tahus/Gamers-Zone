41f87c70dffc84a0872f6060ca03bdfe7c3d792a
Tahus <t.hassouna@hotmail.fr>
2021-09-29 19:04:55 +0200

    architectur DB & Seeding

--- data/gamers_db.sql
+++ data/gamers_db.sql
@@ -23,11 +23,9 @@ CREATE TABLE "articles" (
   "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "title" TEXT NOT NULL,
   "description" TEXT NOT NULL,
-  "url" TEXT NOT NULL,
   "url_image" TEXT NOT NULL,
   "media_type" TEXT NOT NULL,
   -- Notre colonne "categories_id" référencera la colonne "id" de la table "categories"
-  "categories_id"  INTEGER NOT NULL REFERENCES "categories"("id"),
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
@@ -48,7 +46,7 @@ CREATE TABLE "users" (
 -- Dernière table, celle de liaison entre nos catégories et nos articles
 CREATE TABLE "articles_has_categories" (
   "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "article_id" INTEGER NOT NULL REFERENCES "articles"("id") ON DELETE CASCADE,
+  "articles_id" INTEGER NOT NULL REFERENCES "articles"("id") ON DELETE CASCADE,
   "categories_id" INTEGER NOT NULL REFERENCES "categories"("id"),
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
