3df7cb2c74f1bd751790da89a356ed7bd8a5ce7a
Taha <t.hassouna@hotmail.fr>
2022-06-08 22:09:00 +0200

    Profil Page OK & Problem footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
 
-    mode: 'jit',
-
+    
+    darkMode: 'class',
     content: ["./app/views/**/*.{ejs,js}"],
     theme: {
         extend: {
+            
+
             gridTemplateColumns:{
                 //1 column 100px 1 column qui prend tout l'espace qui reste(fractional unit) et une column de 100px
                 "menu": "100px 1fr 100px"
